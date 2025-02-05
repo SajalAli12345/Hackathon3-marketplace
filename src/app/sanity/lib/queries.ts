6ebbefb68dd134d5184876d3c75498a90bf27efd
SajalAli12345 <165483218+SajalAli12345@users.noreply.github.com>
2025-02-05 22:56:01 +0500

    Update queries.ts

--- src/app/sanity/lib/queries.ts
+++ src/app/sanity/lib/queries.ts
@@ -1,4 +1,4 @@
 import { groq } from "next-sanity";
 
 export const allProducts = groq `*[_type == "product"]`;
-export const four = groq `*[_type == "product"][0..3]`;
+//export const four = groq `*[_type == "product"][0..3]`;
