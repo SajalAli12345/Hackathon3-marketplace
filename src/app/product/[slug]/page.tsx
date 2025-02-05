0b8a26c737c1467bb0d6a9553b95660e7e731e15
SajalAli12345 <165483218+SajalAli12345@users.noreply.github.com>
2025-02-05 23:31:34 +0500

    Update page.tsx

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { client } from "@/sanity/lib/client";
+import { client } from "@/app/sanity/lib/client";
 import { Product } from "../../../../types/products";
 import { groq } from "next-sanity";
 import Image from "next/image";
-import { urlFor } from "@/sanity/lib/image";
+import { urlFor } from "@/app/sanity/lib/image";
 import { FaAngleRight, FaStar } from "react-icons/fa";
 import Link from "next/link";
 import { Montserrat } from "next/font/google";
